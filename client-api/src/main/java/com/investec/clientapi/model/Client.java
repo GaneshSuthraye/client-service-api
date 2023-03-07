e6cc2f75385a043392c77bb6c9923279d4279b98
Ganesh <ganesh.suthraya@gmail.com>
2023-03-07 11:21:53 +0200

    Create model class Client

---
+++ client-api/src/main/java/com/investec/clientapi/model/Client.java
@@ -0,0 +1,52 @@
+package com.investec.clientapi.model;
+
+public class Client {
+
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    private String mobileNumber;
+
+    // can create a separate Address model if the entries might be created independently.
+    private String address;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getMobileNumber() {
+        return mobileNumber;
+    }
+
+    public void setMobileNumber(String mobileNumber) {
+        this.mobileNumber = mobileNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
