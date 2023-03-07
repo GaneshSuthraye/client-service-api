8f790022475f3b1235b6f5df229dbb74a873bf03
Ganesh <ganesh.suthraya@gmail.com>
2023-03-07 11:43:36 +0200

    Create ClientService and ClientController

---
+++ client-api/src/main/java/com/investec/clientapi/controller/ClientController.java
@@ -0,0 +1,9 @@
+package com.investec.clientapi.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/client")
+public class ClientController {
+}
---
+++ client-api/src/main/java/com/investec/clientapi/service/ClientService.java
@@ -0,0 +1,29 @@
+package com.investec.clientapi.service;
+
+import com.investec.clientapi.model.Client;
+import com.investec.clientapi.repository.ClientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ClientService {
+
+    @Autowired
+    private ClientRepository clientRepository;
+
+    public Client getClient(String mobileNumber){
+        return clientRepository.findByMobileNumber(mobileNumber);
+    }
+
+    public Client createClient(Client client){
+       return clientRepository.save(client);
+    }
+
+    public Client updateClient(Client client){
+        return clientRepository.save(client);
+    }
+
+
+
+
+}
