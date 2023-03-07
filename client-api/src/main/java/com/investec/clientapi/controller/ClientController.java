9d1ed1b02056ae3fa1fc9b707dd86d47304a72a4
Ganesh <ganesh.suthraya@gmail.com>
2023-03-07 12:05:21 +0200

    add methods in ClientController

--- client-api/src/main/java/com/investec/clientapi/controller/ClientController.java
+++ client-api/src/main/java/com/investec/clientapi/controller/ClientController.java
@@ -1,9 +1,36 @@
 package com.investec.clientapi.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.investec.clientapi.model.Client;
+import com.investec.clientapi.service.ClientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.websocket.server.PathParam;
 
 @RestController
-@RequestMapping(path = "/client")
+@RequestMapping(path = "/clientapi")
 public class ClientController {
+
+    @Autowired
+    private ClientService clientService;
+
+    @GetMapping("/client/{mobileNumber}")
+    public ResponseEntity getClient(@PathParam("mobileNumber") String mobileNumber){
+        return new ResponseEntity(clientService.getClient(mobileNumber), HttpStatus.OK);
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity createClient(@RequestBody Client client){
+        return new ResponseEntity(clientService.createClient(client), HttpStatus.CREATED);
+    }
+
+    @PostMapping("/update")
+    public ResponseEntity updateClient(@RequestBody Client client){
+        return new ResponseEntity(clientService.updateClient(client), HttpStatus.OK);
+    }
+
+
+
 }
